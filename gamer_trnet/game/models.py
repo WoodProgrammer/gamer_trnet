91457da5ea92287905215a86ba770ebdea8d4b93
admanage <asddeneme3@gmail.com>
2017-06-12 13:48:58 +0300

    Media ve oyunlara ait resimler eklendi

--- gamer_trnet/game/models.py
+++ gamer_trnet/game/models.py
@@ -8,6 +8,7 @@ class Game(models.Model):
     game_price = models.FloatField(default = 0)
     genre  = models.CharField(max_length=15)
     created_date = models.DateTimeField(auto_now_add = True)
+    photo  = models.ImageField()
     def __unicode__(self):
         return "GAME CREATED :: >>>>>> {}".format(self.name)
 
