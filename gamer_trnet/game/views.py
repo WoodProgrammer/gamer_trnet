8e1061dd16f18714d32f0aa00e0968a2ecf1f9e5
admanage <asddeneme3@gmail.com>
2017-06-15 01:23:10 +0300

    Deleted all files

--- gamer_trnet/game/views.py
+++
@@ -1,5 +0,0 @@
-from django.shortcuts import render
-from game.models import Game
-def IndexView(request):
-    all_game  = Game.objects.all()
-    return render(request,"index.html",{"all_game":all_game})
