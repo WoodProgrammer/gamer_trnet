25e9f4d5e4706bf1e13f7941da6221c3b72742dd
admanage <asddeneme3@gmail.com>
2017-06-15 02:14:43 +0300

    Gereksiz dosyalar silindi viewda auth için ortam kuruldu

--- epin/epinmain/views.py
+++ epin/epinmain/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render,render_to_response
+from django.http import HttpResponseRedirect
+from django.contrib import auth
 
+import json
 # Create your views here.
+def login(request):
+
+    return render_to_response('login.html')
