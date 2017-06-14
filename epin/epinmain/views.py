7cb6131fc8b78ac7661ad1bae78608a74d0ee3e5
admanage <asddeneme3@gmail.com>
2017-06-15 02:45:46 +0300

    Hooks Deletd Html file of login changed

--- epin/epinmain/views.py
+++ epin/epinmain/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponseRedirect
 from django.contrib import auth
 
 import json
-# Create your views here.
+
 def login(request):
 
     return render_to_response('login.html')
