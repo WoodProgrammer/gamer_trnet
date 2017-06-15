2d77d33bb8a0a9f3bf932dbf3cad5ae4c0e6ca3a
admanage <asddeneme3@gmail.com>
2017-06-15 04:43:10 +0300

    Models bugs fixed

--- epin/epinmain/models.py
+++ epin/epinmain/models.py
@@ -1,7 +1,10 @@
 from django.db import models
 from django.contrib.auth.models import User
 class Genre(models.Model):
-    name = models.CharField(max_length= 225)
+    name = models.CharField(max_length= 225,unique = True)
+    def __str__(self):
+        return self.name
+
 
 class Profile(models.Model):
     user = models.OneToOneField(User,)
@@ -16,7 +19,7 @@ class Game(models.Model):
     popularity = models.IntegerField(default = 1)
     game_logo = models.CharField(max_length=250)
     genre = models.ForeignKey(Genre)
-    def __unicode__(self):
+    def __str__(self):
         return self.name
 
 class Sale(models.Model):
@@ -24,7 +27,8 @@ class Sale(models.Model):
     user = models.ForeignKey(Profile)
     amount = models.FloatField(default = 0)
     created_at = models.DateTimeField()
-
+    def __str__(self):
+        return self.id
 
 class Log(models.Model):
     log_raise = models.CharField(max_length= 225)
